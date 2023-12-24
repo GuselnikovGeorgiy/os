40dc90203e32964427f3c482692c87a6eeb53061
BST2105/Guselnikov <125299947+GuselnikovGeorgiy@users.noreply.github.com>
2023-12-24 03:17:25 +0300

    Update server2.py
    fixed tasks, added checks

--- coursework/server2.py
+++ coursework/server2.py
@@ -1,68 +1,86 @@
-import pickle
 import socket
 import os
 import multiprocessing as mp
 import struct
 import _thread
 import threading
-from psutil import Process
-
+import psutil 
 import subprocess
+import sys
+
 
 #погуглить асинхронный сокет 
 print_lock = threading.Lock()
+
+def get_swap_info():
+    try:
+        swap = psutil.swap_memory().free
+        return swap
+    except Exception as e:
+        print(f"Ошибка получения информации о файле подкачки: {e}")
+        return None
+
+def get_used_memory(data):
+    power = {"1": [1024 ** 0, "Б"],
+            "2": [1024 ** 2, "Мб"],
+            "3": [1024 ** 3, "Гб"]}
+    try:
+        mem = psutil.virtual_memory().used
+        return f"{round(mem / power[data[2]][0], 3)} {power[data[2]][1]}"
+    except Exception as e:
+        print(f"Ошибка получения информации об используемой памяти: {e}")
+        return None
 
 def threaded(conn):
     with conn:
         data = conn.recv(4096).decode("utf-8")
         print(f"Message from client {data}")
-        swap_info = get_swap_info()
-        
-        select_mode = conn.send(f"Выберите единицы измерения для вывода используемой физической памяти:\n1: Байты\n2: Мегабайты\n3: Гигабайты\n".encode("utf-8"))
-        type = int(conn.recv(1024))
-        
-        power = {1: 1024 ** 0, 
-                2: 1024 ** 2, 
-                3: 1024 ** 3}
-
-        if swap_info is not None:
-            swap = str(swap_info / power[type])
-            conn.send(f"Сервер 2: количество свободных байтов подкачки:\n{swap}\n".encode("utf-8"))
-        else:
-            conn.send(f"Сервер 2: ошибка получения информации о файле подкачки.\n".encode("utf-8"))
-
+        client_id = data
+        conn.send("Соединение с Сервером 2 установлено!".encode("utf-8"))
+        while True:
+            data = conn.recv(4096).decode("utf-8")
+            if not data:
+                break
             
-        conn.send("Hello client, i have received your message!".encode("utf-8"))
+            if data[0] == "1":
+                swap_info = get_swap_info()
+                if swap_info is not None:
+                    conn.send(f"Сервер 2: Количество свободных байтов подкачки:\n{swap_info} Б\n".encode("utf-8"))
+                else:
+                    conn.send(f"Сервер 2: Ошибка получения информации о файле подкачки.\n".encode("utf-8"))
+            elif data[0] == "2":
+                used_mem = get_used_memory(data)
+                if used_mem is not None:
+                    conn.send(f"Сервер 2: Объем использованной физической памяти:\n{used_mem}\n".encode("utf-8"))
+                else:
+                    conn.send(f"Сервер 2: Ошибка получения информации о физической памяти.\n".encode("utf-8"))
+            else:
+                conn.send("Недопустимая опция.".encode("utf-8"))    
+        
     print_lock.release()
 
 
-def get_swap_info():
-    try:
-        with open('/proc/meminfo', 'r') as mem_file: # cat /proc/meminfo | grep -i 'swapfree'
-            lines = mem_file.readlines()
-            for line in lines:
-                
-                if line.startswith('SwapFree:'):
-                    return int(line.split()[1]) * 1024
-    except Exception as e:
-        print(f"Ошибка получения информации о файле подкачки: {e}")
-        return None
+
 
 def main():
     host = 'localhost'
     port = 8081
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
+    
     with sock as s:
         #Принимаем подключение и слушаем сокет
-        s.bind((host, port))
+        try:
+            s.bind((host, port))
+        except socket.error as err:
+            if err.errno == socket.errno.EADDRINUSE:
+                print("Сервер 2 уже запущен...")
+                sys.exit(1)
+
         s.listen(5)
         while True:
-            print(f"Сервер ожидает подключения на {host}:{port}")
+            print(f"Сервер 2 ожидает подключения на {host}:{port}")
             conn, addr = s.accept()
-            pid = Process(conn.fileno()).pid
-            print(f"pid: {pid}")
 
             print_lock.acquire()
             _thread.start_new_thread(threaded, (conn,))
